81288e2cf2e18c4343cab49a74cadcbcb5db20b2
melascuco <melascuco@yahoo.es>
2016-02-22 00:56:25 +0100

    Update mailserver

--- SendMail/src/com/melascuco/experimentos/MailSender.java
+++ SendMail/src/com/melascuco/experimentos/MailSender.java
@@ -11,8 +11,8 @@ public class MailSender {
     private static final String SMTP_HOST_NAME = "smtp.gmail.com";
     //private static final int SMTP_HOST_PORT = 465;
     private static final int SMTP_HOST_PORT = 465;
-    private static final String SMTP_AUTH_USER = "jaimmehenson@gmail.com";
-    private static final String SMTP_AUTH_PWD  = "gm5#Puppet";
+    private static final String SMTP_AUTH_USER = "ejemplo@ejemplo.com";
+    private static final String SMTP_AUTH_PWD  = "passejemplo";
 
     public static void Send(String toParam, String msgParam) throws Exception{
         Properties props = new Properties();
@@ -27,15 +27,13 @@ public class MailSender {
         Transport transport = mailSession.getTransport();
 
         MimeMessage message = new MimeMessage(mailSession);
-        message.setSubject("Mest ALARM");
+        message.setSubject("ALARM");
         message.setContent(msgParam,"text/plain");
 
-        //message.setFrom(new InternetAddress("chiron-admin@atos.net"));
-        message.addFrom(new InternetAddress[] { new InternetAddress("jaimmehenson@gmail.com")});
+        message.addFrom(new InternetAddress[] { new InternetAddress("ejemplo@ejemplo.com")});
 		message.addRecipient(Message.RecipientType.TO,
              new InternetAddress(toParam));
-        message.setSender(new InternetAddress("jaimmehenson@gmail.com"));
-        //message.setReplyTo((new InternetAddress("chiron-admin@atos.net")));
+        message.setSender(new InternetAddress("ejemplo@ejemplo.com"));
 
         transport.connect
           (SMTP_HOST_NAME, SMTP_HOST_PORT, SMTP_AUTH_USER, SMTP_AUTH_PWD);
