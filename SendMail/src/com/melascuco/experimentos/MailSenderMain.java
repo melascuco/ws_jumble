521008834f0ce223de37c571ddaf114fd1eb0e99
melascuco <melascuco@yahoo.es>
2016-02-22 00:55:03 +0100

    Update

--- SendMail/src/com/melascuco/experimentos/MailSenderMain.java
+++ SendMail/src/com/melascuco/experimentos/MailSenderMain.java
@@ -3,17 +3,17 @@ package com.melascuco.experimentos;
 public class MailSenderMain {
 
 	public static void main(String[] args) {
-		System.out.println("INICIO ENVÕA EMAIL");
+		System.out.println("INICIO ENV√çA EMAIL");
 		
 		try {
-			MailSender.Send("tugozoenunpozo@hotmail.com", "Texto del correo");
+			MailSender.Send("ejemplo@ejemplo.com", "Texto del correo");
 			System.out.println("Todo correcto");
 		} catch (Exception e) {
-			System.out.println("Ha habido un error, veamos a ver quÈ ha pasado.");
+			System.out.println("Ha habido un error, veamos a ver qu√© ha pasado.");
 			e.printStackTrace();
 		}
 		
-		System.out.println("*FIN ENVÕA EMAIL*");
+		System.out.println("*FIN ENV√çA EMAIL*");
 	}
 
 }
