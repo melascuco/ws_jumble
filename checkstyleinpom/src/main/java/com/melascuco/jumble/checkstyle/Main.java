eafbce09c438acb87f17d2b25de87ec5dc826255
juan.rodriguezp <juan.rodriguezp@atos.net>
2016-10-26 11:28:44 +0200

    Add new project as sample of checkstyle plugin in maven
    See report in checkstyleinpom/target/site/checkstyle.html
    Plugin used can be found here:
    https://maven.apache.org/plugins/maven-checkstyle-plugin/

---
+++ checkstyleinpom/src/main/java/com/melascuco/jumble/checkstyle/Main.java
@@ -0,0 +1,27 @@
+package com.melascuco.jumble.checkstyle;
+
+/**
+ * Class to lauch the sample project.
+ * @author a155841
+ *
+ */
+public final class Main {
+
+  /**
+   * Main method.
+   * @param args array de argumentos
+   */
+  public static void main(final String[] args) {
+    System.out.println(">---- Main START");
+
+    System.out.println("<---- Main END");
+  }
+
+  /**
+   * HideUtilityClassConstructor  avoider.
+   */
+  private Main() {
+    //Just to avoid HideUtilityClassConstructor
+  }
+
+}
---
+++ checkstyleinpom/src/main/java/com/melascuco/jumble/checkstyle/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Provides the classes to test the checkstyle.
+ *
+ * @since 1.0
+ * @see org.apache.maven.plugins.maven-checkstyle-plugin
+ */
+package com.melascuco.jumble.checkstyle;
